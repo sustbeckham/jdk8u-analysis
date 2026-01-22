56bb5e8db1757ac0bb9a90dd7bae9ef68d68fd2e
zhangyingchao <zhangyingchao@shizhuang-inc.com>
2026-01-22 18:52:39 +0800

    update: 处理make问题 https://cr.openjdk.org/\~qpzhang/8235903/webrev.01/jdk.changeset

--- jdk/src/solaris/native/sun/security/jgss/wrapper/NativeFunc.c
+++ jdk/src/solaris/native/sun/security/jgss/wrapper/NativeFunc.c
@@ -28,6 +28,9 @@
 #include <dlfcn.h>
 #include "NativeFunc.h"
 
+/* global GSS function table */
+GSS_FUNCTION_TABLE_PTR ftab;
+
 /* standard GSS method names (ordering is from mapfile) */
 static const char RELEASE_NAME[]                = "gss_release_name";
 static const char IMPORT_NAME[]                 = "gss_import_name";
