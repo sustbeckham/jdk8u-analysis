e22d623aa1b7347cfc970c12d7fccb45eab147fe
zhangyingchao <zhangyingchao@shizhuang-inc.com>
2026-01-26 11:39:47 +0800

    update: 一个验证MIXGC的脚本

--- mixgc.sh
+++ mixgc.sh
@@ -1,3 +1,3 @@
 cd /jdk8u/jdk8u-analysis/test
 /jdk8u/jdk8u-analysis/build/linux-x86_64-normal-server-slowdebug/jdk/bin/javac G1VerifyMixGC.java
-/jdk8u/jdk8u-analysis/build/linux-x86_64-normal-server-slowdebug/jdk/bin/java -Xms4096m -Xmx4096m -XX:G1HeapRegionSize=16m -XX:+UseG1GC G1VerifyMixGC
+/jdk8u/jdk8u-analysis/build/linux-x86_64-normal-server-slowdebug/jdk/bin/java -Xms1024m -Xmx1024m -XX:G1HeapRegionSize=16m -XX:+UseG1GC G1VerifyMixGC
