4d1936f875f79072ac20a628b1165b1133999885
zhangyingchao <zhangyingchao@shizhuang-inc.com>
2026-01-27 14:34:14 +0800

    update: printStackTrace

--- hotspot/src/share/vm/gc_implementation/g1/g1SATBCardTableModRefBS.cpp
+++ hotspot/src/share/vm/gc_implementation/g1/g1SATBCardTableModRefBS.cpp
@@ -162,11 +162,23 @@ G1SATBCardTableLoggingModRefBS(MemRegion whole_heap,
 
 
 
+void printStackTrace() {
+    void* array[10];
+    size_t size;
+    char** strings;
+    size = backtrace(array, 10);
+    strings = backtrace_symbols(array, size);
+    printf("Obtained %zd stack frames.\n", size);
+    for (size_t i = 0; i < size; i++)
+        printf("%s\n", strings[i]);
+    free(strings);
+}
 
 // ** 系统启动时，初始化CardTableRS::CardTableRS
 // ** 初始化 _ct_bs = new G1SATBCardTableLoggingModRefBS 之后
 // ** 进一步调用 _ct_bs->initialize(); 到这里完成初始化工作
 void G1SATBCardTableLoggingModRefBS::initialize(G1RegionToSpaceMapper* mapper) {
+  printStackTrace();
   tty->print_cr("[Fire] G1SATBCardTableLoggingModRefBS::initialize().");
   mapper->set_mapping_changed_listener(&_listener);
 
