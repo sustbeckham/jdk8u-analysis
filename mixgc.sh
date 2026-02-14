66fcbc8f66ca95015609e32a5c6b83defa2b48a7
zhangyingchao <zhangyingchao@shizhuang-inc.com>
2026-02-14 16:25:55 +0800

    update: 用zulu编译

--- mixgc.sh
+++ mixgc.sh
@@ -1,3 +1,3 @@
 cd /jdk8u/jdk8u-analysis/test
-/jdk8u/jdk8u-analysis/build/linux-x86_64-normal-server-slowdebug/jdk/bin/javac -XX:+UseG1GC G1VerifyMixGC.java
+/opt/zulu7.56.0.11-ca-jdk7.0.352-linux_x64/bin/javac -XX:+UseG1GC G1VerifyMixGC.java
 /jdk8u/jdk8u-analysis/build/linux-x86_64-normal-server-slowdebug/jdk/bin/java -Xms4096m -Xmx4096m -XX:+UseG1GC -XX:G1HeapRegionSize=16m -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=512m -XX:+TraceReferenceGC G1VerifyMixGC
